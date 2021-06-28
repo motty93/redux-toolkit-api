afa83d09426c75aa7fc3ab37c11d3d453a825004
motty93 <rdwbocungelt5@gmail.com>
2021-06-28 13:52:15 +0900

    feat: user model validation

--- src/pkg/db/model/user.go
+++ src/pkg/db/model/user.go
@@ -3,7 +3,7 @@ package model
 type User struct {
 	Base
 	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
 	Hobby    string `json:"hobby"`
 }
